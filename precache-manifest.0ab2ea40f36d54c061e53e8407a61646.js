20f52c6d3300cf84b9bf64080bb622cecb142032
Landon Schlangen <landonschlangen@Landons-MacBook-Pro.local>
2020-08-31 10:06:38 -0500

    Updates

---
+++ precache-manifest.0ab2ea40f36d54c061e53e8407a61646.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "618cbd912a342b19dbbfd29410605629",
+    "url": "/CookieClickerGame/index.html"
+  },
+  {
+    "revision": "5dc5614d194ddf9ec366",
+    "url": "/CookieClickerGame/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "39665ce79fb8ad28822a",
+    "url": "/CookieClickerGame/static/js/2.062e3afc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/CookieClickerGame/static/js/2.062e3afc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5dc5614d194ddf9ec366",
+    "url": "/CookieClickerGame/static/js/main.7a260698.chunk.js"
+  },
+  {
+    "revision": "b0dae3a458c2d5a2e2f4",
+    "url": "/CookieClickerGame/static/js/runtime-main.02d3129b.js"
+  }
+]);
